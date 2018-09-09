0431a8957544647f6651e241457c1164f09a24c0
Thiago Kroger <thiagokroger@gmail.com>
2018-09-08 22:23:43 -0300

    light

--- explorers/explorer_ncx.sh
+++ explorers/explorer_ncx.sh
@@ -1,39 +1,109 @@
-sudo apt-get update
-sudo apt install nodejs-legacy -y
-sudo apt-get install npm -y
-sudo apt-get install -y
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "=================================================================="
+echo "CRYPTOSH NodeCircle MN Install"
+echo "=================================================================="
 
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
+#read -p 'Enter your masternode genkey you created in windows, then hit [ENTER]: ' GENKEY
+
+echo -n "Installing pwgen..."
+sudo apt-get install -y pwgen
+
+echo -n "Installing dns utils..."
+sudo apt-get install -y dnsutils
 
 
-cat <<EOF > ./config_mongo.js
-use explorerdb
-db.createUser( { user: "iquidus", pwd: "3xp!0reR", roles: [ "readWrite" ] } )
+#begin optional swap section
+echo "Setting up disk swap..."
+free -h
+sudo fallocate -l 4G /swapfile
+ls -lh /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab sudo bash -c "
+echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
+free -h
+echo "SWAP setup complete..."
+#end optional swap section
+
+echo "Installing packages and updates..."
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install git -y
+sudo apt-get install nano -y
+sudo apt-get install build-essential libtool automake autoconf -y
+sudo apt-get install autotools-dev autoconf pkg-config libssl-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+
+wget https://github.com/nodecircle/NodeCircle/releases/download/v1.0.0/nodecircle-1.0.0-x86_64-linux-gnu.tar.gz
+
+mkdir nodecircle-1.0.0
+tar -zxvf nodecircle-1.0.0-x86_64-linux-gnu.tar.gz
+sudo rm -rf /usr/local/bin/nodecircle-cli
+sudo rm -rf /usr/local/bin/nodecircled
+sudo cp nodecircle-1.0.0/bin/nodecircle-cli /usr/local/bin/
+sudo cp nodecircle-1.0.0/bin/nodecircled /usr/local/bin/
+
+rm -rdf /root/.nodecircle
+mkdir /root/.nodecircle
+
+echo "creating final config..."
+
+cat <<EOF > ~/.nodecircle/nodecircle.conf
+
+server=1
+listen=1
+whitelist=127.0.0.1
+txindex=1
+addressindex=1
+timestampindex=1
+spentindex=1
+rpcallowip=127.0.0.1
+rpcuser=local
+rpcpassword=local@123
+rpcport=18774
+
 EOF
 
-mongo < config_mongo.js
-rm -rf config_mongo.js
+echo "setting basic security..."
+sudo apt-get install fail2ban -y
+sudo apt-get install -y ufw
+sudo apt-get update -y
 
-git clone https://github.com/iquidus/explorer explorer
-cd explorer && npm install --production
+#fail2ban:
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
 
-wget https://raw.githubusercontent.com/thiagokroger/cryptosh/master/explorers/$1_settings.json
-mv $1_settings.json ./settings.json
+#add a firewall
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 18775/tcp
+sudo ufw allow 18774/tcp
+sudo ufw allow 3001/tcp
+sudo ufw allow 80/tcp
+sudo ufw logging on
+sudo ufw status
+echo y | sudo ufw enable
+echo "basic security completed..."
 
-npm install -g forever
-forever start -c "npm start" ./
+echo "restarting wallet with new configs, 30 seconds..."
+nodecircled --daemon
+sleep 30
 
-node scripts/sync.js index reindex
-rm -f tmp/index.pid
 
-cat <<EOF > ./config_cron
-*/1 * * * * cd explorer && /usr/bin/nodejs scripts/sync.js index update > /dev/null 2>&1
-*/5 * * * * cd explorer && /usr/bin/nodejs scripts/peers.js > /dev/null 2>&1
-EOF
-crontab config_cron
-rm config_cron
+echo "nodecircle-cli getinfo"
+nodecircle-cli getinfo
+
+*/1 * * * * cd /root/newexplorer/explorer && /usr/bin/nodejs scripts/sync.js index update > /dev/null 2>&1
+*/2 * * * * cd /root/newexplorer/explorer && /usr/bin/nodejs scripts/sync.js market > /dev/null 2>&1
+*/5 * * * * cd /root/newexplorer/explorer && /usr/bin/nodejs scripts/peers.js > /dev/null 2>&1
--- smartcloud-full.sh
+++ smartcloud-full.sh
@@ -3,9 +3,45 @@ echo "=================================================================="
 echo "Smrtc MN Install"
 echo "=================================================================="
 
+echo -n "Installing pwgen..."
+sudo apt-get update
+sudo apt-get install -y pwgen
+
+echo -n "Installing dns utils..."
+sudo apt-get install -y dnsutils
 
 PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1)
 WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+#begin optional swap section
+echo "Setting up disk swap..."
+free -h
+sudo fallocate -l 4G /swapfile
+ls -lh /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab sudo bash -c "
+echo 'vm.swappiness = 10' >> /etc/sysctl.conf"
+free -h
+echo "SWAP setup complete..."
+#end optional swap section
+
+echo "Installing packages and updates..."
+
+sudo apt-get install git -y
+sudo apt-get install nano -y
+sudo apt-get install libtool -y
+sudo apt-get install autotools-dev pkg-config libssl-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev -y
+sudo apt-get install libzmq3-dev -y
+sudo apt-get install libminiupnpc-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo apt-get install libdb5.3-dev libdb5.3++-dev -y
+
+echo "Packages complete..."
 
 cd ~
 rm -rdf smrtc
@@ -67,6 +103,19 @@ echo "mining info..."
 smrtc-cli getmininginfo
 smrtc-cli stop
 
+
+echo "setting basic security..."
+sudo apt-get install fail2ban -y
+sudo apt-get install -y ufw
+
+#fail2ban:
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
+
+#add a firewall
+sudo ufw allow 9887/tcp
+echo y | sudo ufw enable
+echo "basic security completed..."
 
 echo "restarting wallet with new configs, 30 seconds..."
 smrtcd --daemon
