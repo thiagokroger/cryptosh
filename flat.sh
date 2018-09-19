380debb5c9939c65d4c9bdde5f800bc4856090cb
Thiago Kroger <thiagokroger@gmail.com>
2018-09-19 15:27:40 -0300

    flat

--- flat.sh
+++ flat.sh
@@ -1,19 +1,22 @@
+
 echo "=================================================================="
-echo "PARANOID TRUTH OMEGA MN Install"
+echo "NodeCircle MN Install"
 echo "=================================================================="
-echo "Installing, and will take up to 3 min to run..."
-#read -p 'Enter your masternode genkey you created in windows, then hit [ENTER]: ' GENKEY
 
 echo -n "Installing pwgen..."
-sudo apt-get install -y pwgen 
+sudo apt-get update
+sudo apt-get install -y pwgen
 
 echo -n "Installing dns utils..."
 sudo apt-get install -y dnsutils
 
+PASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1)
+WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
 #begin optional swap section
 echo "Setting up disk swap..."
-free -h 
-sudo fallocate -l 4G /swapfile 
+free -h
+sudo fallocate -l 4G /swapfile
 ls -lh /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -26,21 +29,30 @@ echo "SWAP setup complete..."
 
 echo "Installing packages and updates..."
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
 sudo apt-get install git -y
 sudo apt-get install nano -y
-sudo apt-get install build-essential libtool automake autoconf -y
-sudo apt-get install autotools-dev autoconf pkg-config libssl-dev -y
-sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev -y
-sudo apt-get install libzmq3-dev -y
-sudo apt-get install libminiupnpc-dev -y
+sudo apt-get install libtool -y
+sudo apt-get install autotools-dev pkg-config libssl-dev -y
+sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev libminiupnpc-dev -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install libdb5.3-dev libdb5.3++-dev -y
-sudo apt-get install libdb5.3++ -y
-echo "Flat Install Complete"
+
+echo "Packages complete..."
+
+sudo apt-get install fail2ban -y
+sudo apt-get install -y ufw
+
+sudo systemctl enable fail2ban
+sudo systemctl start fail2ban
+
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw logging on
+sudo ufw status
+echo y | sudo ufw enable
+echo "basic security completed..."
+
+echo "flat complete"
